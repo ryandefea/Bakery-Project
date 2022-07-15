e8732a7758a269fafd6633a957316265a5bedc32
ryandefea <ryan.defea@gmail.com>
2022-07-15 11:42:31 -0700

    Get first test to pass

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -1,82 +1,21 @@
 using System;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ToDoList.Models;
+using Bd.Models;
 
-namespace ToDoList.Tests
+namespace Bd.Tests
 {
   [TestClass]
-  public class ItemTests : IDisposable
+  public class BdTests
   {
 
-    public void Dispose()
-    {
-      Item.ClearAll();
-    }
-
     [TestMethod]
-    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    public void BreadConstructor_CreatesInstanceOfBread_Bread()
     {
-      Item newItem = new Item("test");
-      Assert.AreEqual(typeof(Item), newItem.GetType());
+      Bread newBread = new Bread(1);
+      Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
 
-    [TestMethod]
-    public void GetDescription_ReturnsDescription_String()
-    {
-      // Arrange
-      string description = "Walk the dog.";
-      Item newItem = new Item(description);
-      // Act
-      string result = newItem.Description;
-      // Assert
-      Assert.AreEqual(description, result);
-    }
-
-    [TestMethod]
-    public void SetDescription_SetDescription_String()
-    {
-      // Arrange
-      string description = "Walk the dog.";
-      Item newItem = new Item(description);
-
-      // Act
-      string updateDescription = "Do the dishes";
-      newItem.Description = updateDescription;
-      string result = newItem.Description;
-
-      // Assert
-      Assert.AreEqual(updateDescription, result);
-    }
-
-    [TestMethod]
-    public void GetAll_ReturnsEmptyList_ItemList()
-    {
-      // Arrange
-      List<Item> newList = new List<Item> { };
-
-      // Act
-      List<Item> result = Item.GetAll();
-
-      // Assert
-      CollectionAssert.AreEqual(newList, result);
-    }
-
-    [TestMethod]
-    public void GetAll_ReturnsItems_ItemList()
-    {
-      // Arrange
-      string description01 = "Walk the dog";
-      string description02 = "Wash the dishes";
-      Item newItem1 = new Item(description01);
-      Item newItem2 = new Item(description02);
-      List<Item> newList = new List<Item> { newItem1, newItem2 };
-
-      // Act
-      List<Item> result = Item.GetAll();
-
-      // Assert
-      CollectionAssert.AreEqual(newList, result);
-    }
+   
   }
 }
---
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -0,0 +1,21 @@
+// using System;
+// using System.Collections.Generic;
+// using Microsoft.VisualStudio.TestTools.UnitTesting;
+// using Py.Models;
+
+// namespace Py.Tests
+// {
+//   [TestClass]
+//   public class PyTests
+//   {
+
+//     [TestMethod]
+//     public void ItemConstructor_CreatesInstanceOfItem_Item()
+//     {
+//       Item newItem = new Item("test");
+//       Assert.AreEqual(typeof(Item), newItem.GetType());
+//     }
+
+   
+//   }
+// }
